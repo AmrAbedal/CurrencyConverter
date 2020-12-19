ed8cbdda40a9be42faa81fc9c58667bc43e4d73d
Amr Abdel-Wahab <amr.abedal94@gmail.com>
2020-12-19 21:03:28 +0200

    Solving Fibonacci Problem with two different approch

--- CodeChallenge/main.swift
+++ CodeChallenge/main.swift
@@ -15,7 +15,7 @@ import Foundation
 
 //MARK: - Arnagram Problem
 extension Array where Element == Character {
-    func removeRepeateedCharacters() -> [Character] {
+    func removeRepeatedCharacters() -> [Character] {
         return self.reduce([Character]()) { (result, character) -> [Character] in
             if !result.contains(character) {
                 var result = result
@@ -26,15 +26,39 @@ extension Array where Element == Character {
         }
     }
 }
+
 func isAnagrm(string1: String, String2: String) -> Bool {
     var s1 = string1
     var s2 = String2
     s1.removeAll(where: {$0 == " "})
     s2.removeAll(where: {$0 == " "})
-    let s5 = s1.sorted().removeRepeateedCharacters()
-    let s6 = s2.sorted().removeRepeateedCharacters()
+    let s5 = s1.sorted().removeRepeatedCharacters()
+    let s6 = s2.sorted().removeRepeatedCharacters()
     return s5 == s6
 }
 
 print(isAnagrm(string1: "debit card", String2: "bad credit"))
 print(isAnagrm(string1: "punishments", String2: "nine thumps"))
+
+//MARK: -Fibonacci number
+//Using recursion
+func recursionFibonacci(number: Int) -> Int {
+    if number <= 1 { return number }
+    return recursionFibonacci(number: number - 1) + recursionFibonacci(number: number - 2)
+}
+print(recursionFibonacci(number: 9))
+// dynamicFibonacci
+func dynamicFibonacci(number: Int) -> Int {
+    var fib0 = 0
+    var fib1 = 1
+  
+    if number == 0 { return  fib0}
+    if number == 1 {return fib1 }
+    for _ in 2...number {
+       let fib = fib0 + fib1
+       fib0 = fib1
+       fib1 = fib
+    }
+    return fib1
+}
+print(dynamicFibonacci(number: 9))
