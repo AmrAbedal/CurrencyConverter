e03e5ae871575bd38096baace72c8fd500921e55
Amr Abdel-Wahab <amr.abedal94@gmail.com>
2020-12-19 13:57:15 +0200

    Initial Commit

---
+++ BlackStoneTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackStoneTask
+//
+//  Created by Amr AbdelWahab on 19/12/2020.
+//  Copyright © 2020 BlackStone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
